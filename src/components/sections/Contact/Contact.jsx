d43adfd5455ddbb0c6b5ea6d5de7e49d42acfc6c
Alex Oliveira <iamsralex@gmail.com>
2024-12-06 18:20:23 -0300

    Voltando ao projeto

---
+++ src/components/sections/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import styles from "./Contact.module.css"
+
+const Contact = () => {
+    <section classname={styles.contato}>
+        <div className={styles.container}>
+
+        </div>
+    </section>
+
+}
+
+export default Contact
