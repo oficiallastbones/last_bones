97c3a9f4a55fa6b7d26c09366c8e2393186dbc8f
arturvidalkrause <avidalkrause@gmail.com>
2024-12-23 21:56:16 -0300

    Contatos do whatsapp adicionado

--- src/components/atoms/Card/CardProdutos.jsx
+++ src/components/atoms/Card/CardProdutos.jsx
@@ -2,15 +2,17 @@ import styles from './CardProdutos.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const CardProdutos = ({ id, name, price, imgSrc }) => {
+const CardProdutos = ({ id, name, price, desc, imgSrc }) => {
 	return (
-		<div className={styles.card}>
-			<Image src={imgSrc} width="300" height="400" alt={name} />
-			<div className={styles.desc}>
-				<h2>{name}</h2>
-				<h3>{price}</h3>
+		<Link href={`https://wa.me/5521975566900?text=${desc}`}>
+			<div className={styles.card}>
+				<Image src={imgSrc} width="300" height="400" alt={name} />
+				<div className={styles.desc}>
+					<h2>{name}</h2>
+					<h3>{price}</h3>
+				</div>
 			</div>
-		</div>
+		</Link>
 	)
 }
 
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 // Constantes globais da aplicação
 export const URL_INSTA = "https://www.instagram.com/officiallastbones/"
-export const URL_WHATS = "#"
+export const URL_WHATS = "https://wa.me/5521966822683"
 export const URL_YOUTUBE = "https://www.youtube.com/@lastbones9293/"
